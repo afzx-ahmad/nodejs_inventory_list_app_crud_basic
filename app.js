02bf018c738ab55d54127431f109d6ff8800d7f2
afzx-ahmad <afzx.ahmad@gmail.com>
2021-06-23 11:45:58 +0700

    Add responsive view and showing data from database

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const mysql = require('mysql');
+
+// MYSQL connection
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'nodejs_list_app'
+    }
+);
 
 const app = express();
 
 app.use(express.static('public'));
+app.use('/stylesheets/fontawesome', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
 app.get('/list', (req, res) => {
-    res.render('list.ejs');
+    connection.query('SELECT * FROM items;', (err, results) => {
+        // console.log(results);
+        res.render('list.ejs', {items: results});
+    });
 });
 
 app.listen(3000);
